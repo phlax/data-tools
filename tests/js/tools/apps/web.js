ecd4f428a68125bda1640e8c2914a06be879b6f2
Ryan Northey <ryan@synca.io>
2017-12-30 22:36:51 +0000

    Add javascript tests

---
+++ tests/js/tools/apps/web.js
@@ -0,0 +1,9 @@
+
+import {NotImplementedError} from 'common-errors';
+
+// import L10nWebApp from 'l10n-tools/app/web';
+
+
+test('Web app constructor', () => {
+    // expect(app.app).toBe('bar');
+})
